aa6c25075376981330947c29623d5922293fb2d5
Ede Sufiatti <sufiattie@gmail.com>
2024-10-14 21:39:33 -0300

    0.5-version-game

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 const container = document.getElementById("cards");
 
-for (let i = 0; i < 4; i++) {
-    for (let j = 0; j < 4; j++) {
-        const card = document.createElement('button');
-        card.className = 'cardStyle';
-        container.appendChild(card);
-    }
+const images = [
+    'url1.jpg', 'url2.jpg', 'url3.jpg', 'url4.jpg',
+    'url1.jpg', 'url2.jpg', 'url3.jpg', 'url4.jpg'
+];
+
+for (let i = 0; i < 16; i++) {
+    const card = document.createElement('button');
+    card.className = 'cardStyle';
+
+
+    const img = document.createElement('img');
+    img.src = images[i];
+    img.alt = 'Imagem do Jogo';
+    img.style.width = '100%';
+    img.style.height = 'auto';
+
+    card.appendChild(img);
+    container.appendChild(card);
 }
-
-
